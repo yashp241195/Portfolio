2e8a7a099084902b24dba226bbf44ef2d62dd5e3
Yash Pratap <yashp241195@gmail.com>
2021-12-19 00:56:46 +0530

    questions fixed

---
+++ pages/data/questions.js
@@ -0,0 +1,81 @@
+
+var allQuestions = [
+    {
+        Questions:"things you must know before building software product based startup",
+        tags:"#m #n #x",
+        Answers:`
+        Things you must know before building Software product based Startup <br><br>
+        1.Software solutions are only useful if it boost the operational productivity of the system.<br/><br/>
+        2.The main subject of entrepreneurship is social science, if you dont understand people then there 
+        is no way for you to build anything useful because people are the user of the product and they will only 
+        pay for what they consider valuable. Engineering is only useful in scale. You cant scale non valuable thing 
+        in the market and understanding what is valuable and non valuable requires a deep understanding of human behaviour
+        and civilisations not software engineering. Scaling also requires the person who can maintain the scale of the
+        organisation as well. Process of improving the market value of any product/service is called marketing.<br/><br/>
+        3.If a project leader did the requirement analysis improperly then it cant be fixed from the bottom thats 
+        why meetings are more important than work and main task of a leader is to keep people from different departments 
+        on the same page which means his ability to communicate and learn new things in the given domain should be higher 
+        so that he can make right decisions for the project and involved stakeholders including clients, investors and employees.<br/><br/>
+        `
+    },
+    {
+        Questions:"what is the importance of cultural understanding in business",
+        tags:"#n #g #x",
+        Answers:`
+        You might be wondering like what is the importance of cultural understanding in business. 
+        It took me so long to understand " People are the user of the product and they only pay for 
+        what they consider valuable not what is actually valuable, values are determined by the culture. 
+        Culture is the most important factor that's why when big corporations or big brands enter new country,
+        the first thing they do is destroy culture of another country and promote their own culture so that 
+        they can grow their brands more conveniently. Lets understand this with an example, ever wonder why burger, 
+        pizza etc and all these kinds of fast foods are much costlier than Indian fast foods like vada pav, aaloo tikki, 
+        pani poori etc on an average. Most of the times the cost of raw material are also similar but the difference 
+        in people in Indian think american food is more valuable than Indian food because of rise of influence of western 
+        culture on Indians. If you go more deep in this you will find answers of questions like "Why English is everywhere 
+        even when not needed, Why people assume English speaking person more educated or intellectual than local native language 
+        speaking person" etc etc. <br>
+        <br>
+        Lets take one more example<br>
+        American Brand's Shirt<br>
+        Cost : 1000<br>
+        TAX : 1000<br>
+        Selling Price : 5000<br><br>
+        3000 profit goes to American Investors which they can 
+        use for their own country's development
+        <br><br>
+        Indian Brand's Shirt<br>
+        Cost : 1000<br>
+        TAX : 500<br>
+        Selling Price : 5000<br><br>
+        3500 profit goes to Indian Investors which they can use 
+        for India's development.<br><br>
+        
+        If quality of both products are almost similar and still people buy foreign brands then it means 
+        Indians value american brand more than Indian brands maybe because most products which american brand produce 
+        are better than Indian brands or maybe because Indian brands are not considered as a status symbol. 
+        <br><br>
+        American brands can't influence Indians if Indians don't consider American culture superior then their own culture. 
+        To establish American culture as superior they show a pseudo american culture reality in their movies and TV shows. 
+        America is not what we see american serials or movies just like India is not what we see in Indian movies and serials. 
+        American's show that there life is awesome and Indian's believe it so blindly and in fact the producer director etc 
+        also write that statement "All characters appearing in this work are fictitious. Any resemblance to real persons, living or dead, 
+        is purely coincidental" still people ignore that thing and there is no real way to verify that their life is awesome or not if 
+        one doesn't talk to an American one to one which is very difficult. Indian's blindly believe them due to improper education. 
+        In those serials they promote their own brands which create influence and force Indian's to buy american brands as they are considered cool.
+        `
+    },
+    {
+        Questions:"c",
+        tags:"#m #r #o #x #z",
+        Answers:"z"
+    },
+    {
+        Questions:"d",
+        tags:"#n #o #x",
+        Answers:"p"
+    },
+    
+
+]
+
+
