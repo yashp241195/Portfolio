4b8096e08f579936d0640269f589068cc2589261
Yash Pratap <yashp241195@gmail.com>
2021-12-19 07:46:16 +0530

    questions fixed .

---
+++ pages/data/tutorials.js
@@ -0,0 +1,24 @@
+
+var allTutorials = [
+    {
+        Title:"basic web development with html,css,javascript",
+        tags:"#m #n #x",
+        Blog:`
+        Things you must know before building Software product based Startup <br><br>
+        1.Software solutions are only useful if it boost the operational productivity of the system.<br/><br/>
+        2.The main subject of entrepreneurship is social science, if you dont understand people then there 
+        is no way for you to build anything useful because people are the user of the product and they will only 
+        pay for what they consider valuable. Engineering is only useful in scale. You cant scale non valuable thing 
+        in the market and understanding what is valuable and non valuable requires a deep understanding of human behaviour
+        and civilisations not software engineering. Scaling also requires the person who can maintain the scale of the
+        organisation as well. Process of improving the market value of any product/service is called marketing.<br/><br/>
+        3.If a project leader did the requirement analysis improperly then it cant be fixed from the bottom thats 
+        why meetings are more important than work and main task of a leader is to keep people from different departments 
+        on the same page which means his ability to communicate and learn new things in the given domain should be higher 
+        so that he can make right decisions for the project and involved stakeholders including clients, investors and employees.<br/><br/>
+        `
+    }
+
+]
+
+
