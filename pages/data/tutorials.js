fba0f11d05741c35176acc1c7d3fbc2f1319efdf
Yash Pratap <yashp241195@gmail.com>
2021-12-20 16:37:28 +0530

    tutorials added

--- pages/data/tutorials.js
+++ pages/data/tutorials.js
@@ -2,22 +2,153 @@
 var allTutorials = [
     {
         Title:"basic web development with html,css,javascript",
-        tags:"#m #n #x",
-        Blog:`
-        Things you must know before building Software product based Startup <br><br>
-        1.Software solutions are only useful if it boost the operational productivity of the system.<br/><br/>
-        2.The main subject of entrepreneurship is social science, if you dont understand people then there 
-        is no way for you to build anything useful because people are the user of the product and they will only 
-        pay for what they consider valuable. Engineering is only useful in scale. You cant scale non valuable thing 
-        in the market and understanding what is valuable and non valuable requires a deep understanding of human behaviour
-        and civilisations not software engineering. Scaling also requires the person who can maintain the scale of the
-        organisation as well. Process of improving the market value of any product/service is called marketing.<br/><br/>
-        3.If a project leader did the requirement analysis improperly then it cant be fixed from the bottom thats 
-        why meetings are more important than work and main task of a leader is to keep people from different departments 
-        on the same page which means his ability to communicate and learn new things in the given domain should be higher 
-        so that he can make right decisions for the project and involved stakeholders including clients, investors and employees.<br/><br/>
+        tags:"#html #css #javascript #web #frontend",
+        Blog:` 
+        
+        Web browser read H.T.M.L. (hypertext markup language) code and create D.O.M. (document object model) tree corresponding to that in the memory. CSS helps you to style components of D.O.M tree
+        and then javascript helps you manipulate the DOM tree in memory programmatically.
+        <br><br>
+        <b>HTML</b> : <xmp><div id="#mydiv">Hello</div></xmp>
+        <b>CSS</b> : <xmp> #mydiv{color:"red"} </xmp>
+        <b>Javascript</b> : <xmp> document.getElementById("mydiv").innerHTML+=" World" </xmp>
+        <b>Output</b>:<br/><br> 
+        <div style="color:red">Hello World</div>
+        <br> 
+        <h3>Lectures:</h3>  
+        <style>
+        table, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+            padding:3px;
+            border-color: lightgray;
+          }
+        </style>
+        <table style="width:100%;border: 1px solid; ">
+        <tr><th>Topic</th><th>Category</th><th>URL</th></tr>
+        <tr><td>HTML</td><td>Web Development (Frontend)</td><td><a target="_blank" href="https://www.youtube.com/watch?v=E3ByCRqE7Lo">html-codewithharry</a></td></tr>
+        <tr><td>CSS</td><td>Web Development (Frontend)</td><td><a  target="_blank" href="https://www.youtube.com/watch?v=u5-K_ua9sOw">css-codewithharry</a></td></tr>
+        <tr><td>JavaScript</td><td>Web Development (Frontend)</td><td><a  target="_blank" href="https://www.youtube.com/watch?v=hKB-YGF14SY">js-codewithharry</a></td></tr>
+        <tr><td>Namaste JavaScript</td><td>Javascript (Core)</td><td><a  target="_blank" href="https://www.youtube.com/watch?v=pN6jk0uUrD8&list=PLlasXeu85E9cQ32gLCvAvr9vNaUccPVNP">namaste-js-akshay-saini</a></td></tr>
+        <tr><td>W3schools</td><td>Tutorial/IDE for html,css,javascript</td><td><a  target="_blank" href="https://www.w3schools.com/html/html_basic.asp">w3schools</a></td></tr>
+        </table>
         `
-    }
+    },
+    {
+        Title:"basic programming in python",
+        tags:"#python #basicprogramming",
+        Blog:`
+        <h4>Basic programming in python</h4>
+        4 Fundamentals of basic programming : <br>
+        (i) basic operations<br>
+        (ii) functions<br>
+        (iii) loop<br>
+        (iv) recursion<br> 
+        <pre>
+        print("Hello world")
+        # arithmetic operations
+        x = 2
+        y = 3
+        z1 = x+y
+        print("sum of ",x,"+",y,"= ",z1)
+
+        # Function: for reuse same code 
+        def sq(x):
+            return x*x
+
+        z2 = sq(x) + sq(y)
+        print("sum of sq(",x,") + sq(",y,") = ",z2)
+
+        # sum of first 5
+        # (i) repeat sum
+        fsum = 0
+        fsum = fsum + 1
+        fsum = fsum + 2
+        fsum = fsum + 3
+        fsum = fsum + 4
+        fsum = fsum + 5
+
+        print("sum of first 5 with repeat add",fsum)
+
+        # (ii) for loop
+        fsum = 0
+        for i in range(1, 5+1):
+            fsum = fsum + i
+
+        print("sum of first 5 with for loop",fsum)
+
+        # (iii) with recursion
+        def calcSum(n):
+            if(n == 0):
+                return 0
+            return n + calcSum(n-1)
+
+        fsum = calcSum(5)
+        print("sum of first 5 with recursion",fsum)
+        </pre>
+        <h4>Output</h4>
+        <pre>
+        Hello world
+        sum of  2 + 3 =  5
+        sum of sq( 2 ) + sq( 3 ) =  13
+        sum of first 5 with repeat add 15
+        sum of first 5 with for loop 15
+        sum of first 5 with recursion 15
+        </pre>
+        <br> 
+        <h3>Lectures:</h3>  
+        <style>
+        table, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+            padding:3px;
+            border-color: lightgray;
+          }
+        </style>
+        <table style="width:100%;border: 1px solid; ">
+        <tr><th>Topic</th><th>Category</th><th>URL</th></tr>
+        <tr><td>Python Beginner Full</td><td>Programming</td><td><a target="_blank" href="https://www.youtube.com/watch?v=gfDE2a7MKjA">python-codewithharry</a></td></tr>
+        </table>
+        `
+    },
+    {
+        Title:"Linux commandline, bash ",
+        tags:"#linux #basicprogramming",
+        Blog:`
+        <h4>Basic Linux Commands</h4>
+        The basic approach to use commandline tool is :<br>
+        Use command to view things or print things so
+        that you can find things then select the thing you find 
+        and then operate.<br><br>
+        <h4>Basic Commands</h4>
+        echo <text> : print the text in terminal<br>
+        ls : list of files present in folder<br>
+        ls -a: list of files present in folder and 
+        -a flag will including hidden files as well<br>
+        cd : change directory<br>
+        mkdir : make a folder/directory<br>
+        touch : create the file<br>
+        gedit <filename> : open the file in gedit<br>
+        history : list of all commands you entered earlier<br>
+        clear : clear the screen<br>
+        sudo <command> : run command as super user<br>
+        sudo apt-get install <package-name> : install package from apt-get<br>
+        sudo snap install <package-name> : install package from snapcraft <br>
+        <br>
+        <h3>Lectures:</h3>  
+        <style>
+        table, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+            padding:3px;
+            border-color: lightgray;
+          }
+        </style>
+        <table style="width:100%;border: 1px solid; ">
+        <tr><th>Topic</th><th>Category</th><th>URL</th></tr>
+        <tr><td>Python Beginner Full</td><td>Programming</td><td><a target="_blank" href="https://www.youtube.com/watch?v=gfDE2a7MKjA">python-codewithharry</a></td></tr>
+        </table>
+        `
+    },
 
 ]
 
