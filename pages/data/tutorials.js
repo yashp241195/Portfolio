f37a4153604f40ca4e99ac04ad2e8dbd5d245cad
Yash <yashp241195@gmail.com>
2022-09-28 00:27:52 +0530

    Update tutorials.js

--- pages/data/tutorials.js
+++ pages/data/tutorials.js
@@ -2055,6 +2055,27 @@ var allTutorials = [
         </p>
         </p>
         <br>
+	<p>Start-up Pitch Deck Guide:</p>
+	<p>1. Explaining the Problem : Explain it with short bullet points if possible.</p>
+	<p>A Quick visual story which explain the problem statement correctly might be helpful.</p>
+	<p>If investor doesn't consider the problem you are talking about as a &quot;real problem&quot; then it doesn't matter whether you are right or wrong because investors aren't the great economist or genius like Socrates they are regular people with money.</p>
+	<p>PS : Problem means what people consider as problem not their actual or real problem.</p>
+	<p>If people were that smart to identify their real problems then there might not be any existence of world war, social injustice, climate change, world hunger etc.</p>
+	<p>Most start-up pitch got rejected because founders don't do research about &quot;how does an ideal investor should look like for current phase of business&quot; just like you do it for your business &quot;how does an ideal customer should look like for current stage of business&quot;.</p>
+	<p>You might think that &quot;negative feedback from investors means you should quit&quot; but you will face the similar challenge while dealing with customers later as they will suggest you to add all unnecessary things to your product or service and even give you very negative feedback without any proper reason but it is your responsibility as a business owner to understand the limitations of your marketing funnel (you can't sell computers hardware inside restaurant) and current resources.</p>
+	<p>Say you started Pizza restaurant and suddenly customer demand Dosa. Will that be feasible for you to add that in your menu? No. You can arrange it by some other local shop or food delivery app but you can't add it in your menu or service.</p>
+	<p>You can't be stupid or naive about how society works, entrepreneurship is primarily a social science subject.</p>
+	<p>2. Existing Solutions/Products : Mention all the major flaws in the existing solution/product.</p>
+	<p>Your answer to the &quot;major flaws&quot; will tell investor about your understanding regarding the problem space and one problem space can contain plenty of products or services. If your problem space matches the problem space investor invests majorly then there might be very high chances that you will hear back from investors even if your idea isn't good enough because investor help you to get in the network of other people operating in similar problem space which open door for new possibilities regarding products, systems, teams, investors etc.</p>
+	<p>3. Your Solution/Product : Your solution and product must be able to solve all the major flaws you mentioned above.</p>
+	<p>4. Traction : Your current traction of product will convince investor about your execution abilities.</p>
+	<p>5. Market : Market size must be big enough and your product must be able to produce 10X or more profits in 5-10 years.</p>
+	<p>6. Business Model : Mention exactly how you are planning to produce profits or mention your key revenue sources.</p>
+	<p>7. Team : Why you? What makes you and your team qualified for solving a particular problem.</p>
+	<p>8. Fund-raising plan and objectives : Mention in bullet points what will you do with the money you are asking for.</p>
+	<p>Now, You can ask for investments, make negotiations and close the deal. </p>
+	<br/>
+	Checkout more about pitch deck here : <a target="_blank" href="https://github.com/yashp241195/startup_pitch_deck">startup-pitch-deck</a>
         <h3>Lectures:</h3>
         <p>The only intention of lectures is to let you know the topics
         you are supposed to cover in order to gain the implementation abilities. 
